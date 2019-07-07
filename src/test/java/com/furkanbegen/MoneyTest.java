b3b10c5089c45aa7c63cb57a87cd8f7918d31e22
Furkan Beğen <furkanbegen@gmail.com>
2019-07-07 22:34:55 +0300

    remove subclasses

--- src/test/java/com/furkanbegen/MoneyTest.java
+++ src/test/java/com/furkanbegen/MoneyTest.java
@@ -13,34 +13,26 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
+    void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
+
+        Money fiveFranc = Money.franc(5);
+        assertEquals(Money.franc(10), fiveFranc.times(2));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-    }
-
-    @Test
-    void testMultiplicationFrank() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-     @Test
-    void testEqualityFrank() {
         assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(8));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
-        assertEquals("CHF", Franc.franc(1).currency());
+        assertEquals("CHF", Money .franc(1).currency());
     }
 }
